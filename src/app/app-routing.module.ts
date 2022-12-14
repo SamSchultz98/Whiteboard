fff87162449fb42551211b328b3b8f968d1d888a
SamSchultz98 <sam.schultz@outlook.com>
2022-12-14 09:42:55 -0500

    HomeAndE404Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Misc/home/home.component';
+import { AboutComponent } from './Misc/about/about.component';
+import { E404Component } from './Misc/e404/e404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:"/home",pathMatch:"full"},
+  {path:"home", component:HomeComponent},
+  {path:"**",component:E404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
